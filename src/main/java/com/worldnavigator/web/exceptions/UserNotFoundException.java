072eb472ce79084bde5337613ad3a557511739dc
Mohammad Shakhatreh <hmodaa.sha@hotmail.com>
2020-08-16 11:49:54 +0300

    Add simple exception handling

--- src/main/java/com/worldnavigator/web/exceptions/UserNotFoundException.java
+++ src/main/java/com/worldnavigator/web/exceptions/UserNotFoundException.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
-    public UserNotFoundException() {}
-
     public UserNotFoundException(String message) {
         super(message);
     }
